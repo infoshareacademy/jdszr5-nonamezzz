5c8f96bec942cddc1edb618ffc012d734c580327
Michał Mielniczek <micmielniczek@gmail.com>
2021-10-07 15:02:21 +0200

    task 1 and task 8 done, 3 sql script added (1 insert sql script as alternative) Task 1 - routes_distance_weather scripts, task 8 - distance script

---
+++ distance.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c44722803b330f17767a81196755ca22a9e9b9931129d7dcbcf9eb9c376f0b71
+size 2571
---
+++ routes_distance_weather.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5aafc86cdbefb53ab5a0cc4443e3c5d541797cc12fca7248c3233017585942f5
+size 1512
